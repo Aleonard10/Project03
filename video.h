9dcad20870b5935c32671c85e2b3fc8db8d36892
Aleonard10 <31227161+Aleonard10@users.noreply.github.com>
2018-02-27 16:32:38 -0800

    Create video.h

---
+++ video.h
@@ -0,0 +1,20 @@
+using namespace std;
+#ifndef VIDEO_H
+#define VIDEO_H
+class video
+{
+    public:
+      video(string title, string url, string comment, float length, int rating); //prototype function for constructor
+      void print(); //prototype function for printing the output
+      bool longer(video* p); //prototype function for testing difference in length between 2 videos
+      bool alphabetical(video* p); //prototype function for testing difference in alphabetical order between 2 videos
+      bool better(video* p); //prototype function for testing difference in rating between 2 videos
+    private:
+      string m_title; //member variable for title
+      string m_url; //member variable for url
+      string m_comment; //member variable for comment
+      float m_length; //member variable for length
+      int m_rating; //member variable for rating
+
+};
+#endif
